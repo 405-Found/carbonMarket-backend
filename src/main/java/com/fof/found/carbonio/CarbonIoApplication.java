97b8c633e07b83eeaaeaf4f4ab4cafa3dce960d0
Roeschach <HAIXIE@student.unimelb.eud.au>
2022-04-21 14:08:20 +1000

    first structure

---
+++ src/main/java/com/fof/found/carbonio/CarbonIoApplication.java
@@ -0,0 +1,13 @@
+package com.fof.found.carbonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonIoApplication.class, args);
+    }
+
+}
