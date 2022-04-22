5e11fe9430febc231cad90c6c329c8895e822131
Roeschach <HAIXIE@student.unimelb.eud.au>
2022-04-22 18:01:27 +1000

    add tip

--- src/main/java/com/fof/found/carbonio/entity/Tip.java
+++ src/main/java/com/fof/found/carbonio/entity/Tip.java
@@ -1,8 +1,10 @@
 package com.fof.found.carbonio.entity;
 
 import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
+@Document(indexName = "tip")
 public class Tip {
     private String id;
     private String content;
